11210c20874b6acd978feffe1f4b99758b300a10
Raphael Anyanwu <anyanwuraphaelc@gmail.com>
2025-09-02 09:54:38 +0100

    update core services

--- src/Throttlr.Api.RateLimit/Core/SystemClock.cs
+++ src/Throttlr.Api.RateLimit/Core/SystemClock.cs
@@ -5,9 +5,6 @@ using RateLimit.Throttlr.Abstractions;
 
 namespace RateLimit.Throttlr.Core
 {
-    /// <summary>
-    /// Default implementation of <see cref="ISystemClock"/> that uses <see cref="DateTimeOffset.UtcNow"/>.
-    /// </summary>
     public sealed class SystemClock : ISystemClock
     {
         /// <inheritdoc />
